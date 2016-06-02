9782241e24f79f3364168e06bf1063fd01a07f0c
Stefanie Hansen <stefuhnee@gmail.com>
2016-06-02 14:39:25 -0700

    changed server port

--- stefanie-hansen/server.js
+++ stefanie-hansen/server.js
@@ -25,6 +25,6 @@ app.all('*', (req, res) => {
   res.sendStatus(404);
 });
 
-app.listen(3000, () => {
-  console.log('listening on 3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('listening on ' + process.env.PORT || '3000');
 });
